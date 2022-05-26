f874fafcf98440f3458d16c47254e4819c572180
Kaua Fortes <kaua.fortes03@gmail.com>
2022-05-25 21:31:10 -0300

    25/05

--- web/bases/listas/main.js
+++ web/bases/listas/main.js
@@ -1,48 +1,23 @@
-//javascript: uma linguagem de programação
+const buttonSendMenssage = document.getElementById('buttonSendMenssage')
 
-//POO: programação orientada á objetos
-//Atributos e Métodos (funções)
+const messages = []
 
-//Exemplos de atributos: cor, altura, largura, etc.
-//Exemplos de Métodos/Funções: freiar, acelerar, virarAdireita
+function addMessage(event) {
+  event.preventDefault() //desabilita o evento padrão do elemento
 
-let nomeVarialvel = 1
+  const inputName = document.getElementById('name')
+  const inputEmail = document.getElementById('email')
+  const inputMens = document.getElementById('mens')
 
-//A convenção da declaração de variavel deve ser seguida:
-// Variavel NÃO DEVE iniciar com NÚMEROS
-// Variavel NÃO PODE iniciar com caracteres especiais
-// Variavel NÃO PODE iniciar com letra caixa ALTA: 
-// exemplo errado: Banana
-// exemplo correto: banana
-// variaveis devem possuir um valor semântico (fazer sentido)
-// exemplos ruim: banana, a, b, c, x, y, n1, n2, n3
-// exemplos bom: numeroDeParcelas
-// exemplos nao pode ter acento
-// funções deve ser seguir asmesmas convenções e devem ter valor
-
-/* exemplos */
-let nomeUsuario = 1;
-
-// 1* Método de criar uma função, atribui-lá à uma variável
-let somaValores = function () {
-  console.log('somando valores')
-}
-
-//chmar/invocar/conjugar/acionar
-somaValores();
-
-// 2° metodo de declaração de variável named function
-function subtraiValores() {
-  console.log('subtrai')
-}
-subtraiValores();
-
-// 3° metodo
-const carro = {
-  cor: 'vermelha', // atributo
-  andar: function(){ // metodo
-    console.log('andando')
+  const mens = {
+    name: inputName.value,
+    email: inputEmail.value,
+    mens: inputMens.value
   }
+  messages.push(mens)
+
+  document.getElementById('forme').reset()
+  console.log('imprimeir vetor', mens)
 }
 
-carro.andar();
+buttonSendMenssage.addEventListener('click', addMessage)
