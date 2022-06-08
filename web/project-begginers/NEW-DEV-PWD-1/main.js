daa7ef7786792a4b7f2a0cb7134a86d3e2805387
Kaua Fortes <kaua.fortes03@gmail.com>
2022-06-07 21:27:25 -0300

    07/06

--- web/project-begginers/NEW-DEV-PWD-1/main.js
+++ web/project-begginers/NEW-DEV-PWD-1/main.js
@@ -1,8 +1,121 @@
 let record = []
 
+const iconEdit = document.createElement('i')
+iconEdit.setAttribute('class', 'fas fa-edit')
+iconEdit.setAttribute('title', 'Editar')
+iconEdit.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;')
 
-const addPeople = (event) => {
-  event.preventDefault();
+const iconRemove = document.createElement('i')
+iconRemove.setAttribute('class', 'fas fa-trash')
+iconRemove.setAttribute('title', 'Remover')
+iconRemove.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;')
+
+
+function onClickRemove(lineToRemove) {
+  lineToRemove.remove();
+}
+
+function onClickEdit (lineEditing) {
+  lineEditingInMoment = lineEditing;
+
+  const [firtsName, lastName, address, city, cod, country, phoneNumber, email] = lineEditing.childNodes;
+  document.getElementById('firtsName')
+    .value = firtsName.innerHTML;
+
+  document.getElementById('lastName')
+    .value = lastName.innerHTML;
+
+  document.getElementById('address')
+    .value = address.innerHTML;
+
+  document.getElementById('city')
+    .value = city.innerHTML;
+
+  document.getElementById('cod')
+    .value = cod.innerHTML;
+
+  document.getElementById('country')
+    .value = country.innerHTML;
+
+  document.getElementById('phoneNumber')
+    .value = phoneNumber.innerHTML;
+
+  document.getElementById('email')
+    .value = email.innerHTML;
+} 
+
+const loadPeoples = () => {
+  const itemsJaArmazenados = localStorage.getItem('pessoas lsitadas')
+  return itemsJaArmazenados ? JSON.parse(itemsJaArmazenados) : []
+}
+
+const span = () => {
+  const span = document.createElement('span')
+
+  span.appendChild(iconEdit)
+  span.appendChild(iconRemove)
+
+
+  li.appendChild(span);
+
+  li.setAttribute('id', `line${countRow}`);
+  countRow += 1;
+
+  iconEdit.setAttribute('onclick', `onClickEdit(${span.parentElement.id});`);
+  iconRemove.setAttribute('onclick', `onClickRemove(${span.parentElement.id});`)
+
+  if (lineEditingInMoment) {
+    const [firtsNameToUpdate, lastNameToUpdate, addressToUpdate, cityToUpdate, codToUpdate, countryToUpdate, phoneNumberToUpdate, emailToUpdate] = 
+      lineEditingInMoment.childNodes;
+    
+      firtsNameToUpdate.innerHTML = message.firtsName;
+      lastNameToUpdate.innerHTML = message.lastName;
+      addressToUpdate.innerHTML = message.addressTo;
+      cityToUpdate.innerHTML = message.city;
+      codToUpdate.innerHTML = message.cod;
+      countryToUpdate.innerHTML = message.country;
+      phoneNumberToUpdate.innerHTML = message.phoneNumber;
+      emailToUpdate.innerHTML = message.email;
+
+    lineEditingInMoment = null;
+  } else {
+    tbody-messages.appendChild(li);
+  } 
+  return span
+}
+
+const listPeoples = () => {
+  const peoples = loadPeoples()
+
+  let ul = document.querySelector('ul')
+  if (ul) {
+    ul.remove()
+  }
+
+  ul = document.createElement('ul')
+
+  peoples.forEach((item, index) => {
+    const li = document.createElement('li')
+    li.innerHTML = 
+    `primeiro nome: ${item.firtsName},
+    sobrenome: ${item.lastName}, 
+    endereço: ${item.address},
+    cidade: ${item.city}, 
+    codigo: ${item.cod},
+    pais: ${item.country},
+    numero: ${item.phoneNumber},
+    email do usuario ${item.email}`
+
+    li.appendChild(span())
+    ul.appendChild(li)
+  })
+  document.getElementById('tbody-messages').appendChild(ul)
+}
+
+listPeoples()
+
+const addPeople = event => {
+  event.preventDefault()
   const people = {
     firtsName: document.getElementById('firtsName').value,
     lastName: document.getElementById('lastName').value,
@@ -11,42 +124,21 @@ const addPeople = (event) => {
     cod: document.getElementById('cod').value,
     country: document.getElementById('country').value,
     phoneNumber: document.getElementById('phoneNumber').value,
-    email: document.getElementById('email').value,
+    email: document.getElementById('email').value
   }
 
-  const itemsJaArmazenados = localStorage.getItem('people')
-
-  record = itemsJaArmazenados ? JSON.parse(itemsJaArmazenados) : []
+  record = loadPeoples()
 
   record.push(people)
 
-  localStorage.setItem('people', JSON.stringify(record))
+  //JSON transforma a stringify em obejto ou vice versa
+  localStorage.setItem('pessoas lsitadas', JSON.stringify(record))
 
-  document.querySelector('form').reset();
-
-  const items = localStorage.getItem('people')
-
-  const itemsSerialized = JSON.parse(items)
+  document.querySelector('form').reset()
 
 
-  //ul[{}, {}] - length = 2
-  //itemsSerialized[{} {} {}] - length = 3
-  let ul = document.querySelector('ul');
-  if(ul) {
-    ul.remove()
-  }
-  
-  ul = document.createElement('ul')
 
-  console.log('node list ul', ul.childNodes)
-  itemsSerialized.forEach((item, index) => {
-      const li = document.createElement('li')
-      li.innerHTML = `primeiro nome: ${item.firstname}/ sobrenome: ${item.lastname}/ endereço: ${item.address}/ cidade: ${item.city}/ codigo: ${item.cod}/ pais: ${item.country}/ numero: ${item.phoneNumber}/ emailusuario ${item.email}/`
-      console.log('ITEMS', item)
-    
-    ul.appendChild(li)
-  })
-  document.getElementById('tbody-messages').appendChild(ul)
+  listPeoples()
 }
-  const addButton = document.getElementById('addButton');
-  addButton.addEventListener('click', addPeople)
+const addButton = document.getElementById('addButton')
+addButton.addEventListener('click', addPeople)
